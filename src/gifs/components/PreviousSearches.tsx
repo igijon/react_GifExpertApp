265b949b2550bad71a91b090896fe0fd6a1bab91
Inma Gijón <igijoninf@gmail.com>
2025-11-28 12:00:15 +0100

    Manejo de estado. Búsquedas previas

--- src/gifs/components/PreviousSearches.tsx
+++ src/gifs/components/PreviousSearches.tsx
@@ -2,17 +2,18 @@ import type { FC } from "react";
 
 interface Props {
   searches: string[];
+  onLabelClicked: (term: string) => void;
 }
-export const PreviousSearches: FC<Props> = ({searches}) => {
+export const PreviousSearches: FC<Props> = ({ searches, onLabelClicked }) => {
   return (
     <div className="previous-searches">
       <h2>Búsquedas Previas</h2>
       <ul className="previous-searches-list">
-        {
-        searches.map(term => (
-          <li key={term}>{term}</li>
-        ))
-      }
+        {searches.map((term) => (
+          <li key={term} onClick={() => onLabelClicked(term)}>
+            {term}
+          </li>
+        ))}
       </ul>
     </div>
   );
