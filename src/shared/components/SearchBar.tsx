d232515d1aaf8fe601d71ef03f78b49c6339aef8
Inma Gijón <igijoninf@gmail.com>
2025-12-01 12:42:12 +0100

    UseEffect - Debouncer

--- src/shared/components/SearchBar.tsx
+++ src/shared/components/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { useState, type KeyboardEvent } from "react";
+import { useEffect, useState, type KeyboardEvent } from "react";
 
 interface Props {
   placeholder?: string;
@@ -6,7 +6,24 @@ interface Props {
 }
 
 export const SearchBar = ({ placeholder = "Buscar", onQuery }: Props) => {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('');
+
+  //Se busca que los efectos hagan una única tarea específica
+  //Se dispara cuando el componente se monta
+  useEffect(() => {
+
+    const timeoutId = setTimeout(() => {
+      //Cada vez que query cambie, quiero llamar a onQuery
+      //cuando algo cambia, se ejecuta useEffect
+      onQuery(query);
+    }, 700);
+
+    return () => {
+      //Aquí va la limpieza del efecto
+      //Se va a llamar cuando el componente deja de existir
+      clearTimeout(timeoutId);
+    };
+  }, [query, onQuery]); // Especificamos en un array las dependencias que deben detonar este useEffect
 
   const handleSearch = () => {
     onQuery(query);
@@ -21,7 +38,6 @@ export const SearchBar = ({ placeholder = "Buscar", onQuery }: Props) => {
 
   return (
     <div className="search-container">
-      <h1>{query}</h1>
       <input
         type="text"
         placeholder={placeholder}
