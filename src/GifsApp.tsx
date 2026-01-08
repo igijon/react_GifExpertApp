21e8a46752499e1750702cd95aaed5c745fc03d0
Inma Gijón <igijoninf@gmail.com>
2026-01-08 12:25:48 +0100

    Listado de Gifs

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { GifList } from "./gifs/components/GifList";
 import { PreviousSearches } from "./gifs/components/PreviousSearches";
-import { mockGifs } from "./mock-data/gifs.mock";
 import { CustomHeader } from "./shared/components/CustomHeader";
 import { SearchBar } from "./shared/components/SearchBar";
 import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.action";
+import type { Gif } from "./gifs/interfaces/gif.interface";
 
 export const GifsApp = () => {
-  const [previousTerms, setPreviousTerms] = useState([] as string[]);
+  const [gifs, setGifs] = useState<Gif[]>([]);
+  const [previousTerms, setPreviousTerms] = useState<string[]>([]);
 
   const handleTermClicked = (term: string = '') => {
     console.log(`Term clicked: ${term}`);
@@ -22,7 +23,7 @@ export const GifsApp = () => {
     setPreviousTerms([query, ...previousTerms].splice(0, 7));
 
     const gifs = await getGifsByQuery(query);
-    console.log(gifs);
+    setGifs(gifs);
   };
 
   return (
@@ -46,7 +47,7 @@ export const GifsApp = () => {
       />
 
       {/* Gifs */}
-      <GifList gifs={mockGifs} />
+      <GifList gifs={gifs} />
     </>
   );
 };
