37571709ffdcabc31eb26f95ef1f0263d287b934
Inma Gijón <igijoninf@gmail.com>
2026-01-12 13:28:29 +0100

    UseRef para la caché

--- src/gifs/hooks/useGifs.tsx
+++ src/gifs/hooks/useGifs.tsx
@@ -1,20 +1,21 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { getGifsByQuery } from "../actions/get-gifs-by-query.action";
 import type { Gif } from "../interfaces/gif.interface";
 
 //Por ahora dejamos la caché aquí porque si lo metemos dentro del componente
 //cada vez que se renderice el componente se perdería la caché.
-const gifsCache: Record<string, Gif[]> = {};
+// const gifsCache: Record<string, Gif[]> = {};
 
 export const useGifs = () => {
   const [gifs, setGifs] = useState<Gif[]>([]);
   const [previousTerms, setPreviousTerms] = useState<string[]>([]);
 
-  
+  const gifsCache = useRef<Record<string, Gif[]>>({});
+
   const handleTermClicked = async (term: string = "") => {
-    if (gifsCache[term]) {
-        setGifs(gifsCache[term]);
-        return;
+    if (gifsCache.current[term]) {
+      setGifs(gifsCache.current[term]);
+      return;
     }
     const gifs = await getGifsByQuery(term);
     setGifs(gifs);
@@ -31,8 +32,7 @@ export const useGifs = () => {
     const gifs = await getGifsByQuery(query);
     setGifs(gifs);
 
-    gifsCache[query] = gifs;
-    
+    gifsCache.current[query] = gifs;
   };
 
   return {
