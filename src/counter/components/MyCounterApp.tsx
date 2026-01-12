7e54b5c3e3e4a0794527e62a1c3d2034a741cfe2
Inma Gijón <igijoninf@gmail.com>
2026-01-12 12:33:27 +0100

    Custom Hooks

--- src/counter/components/MyCounterApp.tsx
+++ src/counter/components/MyCounterApp.tsx
@@ -1,22 +1,19 @@
-import { useState } from "react";
+import { useCounter } from "../hooks/useCounter";
 
 export const MyCounterApp = () => {
-  const [ counter, setCounter]  = useState(5);
+  const { counter, handleAdd, handleSubtract, handleReset } = useCounter();
 
-  const handleAdd = () => setCounter(counter + 1);
-    const handleSubtract = () => setCounter(prevState => prevState - 1);
-    const handleReset = () => setCounter(0);
   return (
     <div
-      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
     >
       <h1>counter: {counter}</h1>
 
-      <div style={{ display: 'flex', gap: '10px' }}>
+      <div style={{ display: "flex", gap: "10px" }}>
         <button onClick={handleAdd}>+1</button>
         <button onClick={handleSubtract}>-1</button>
         <button onClick={handleReset}>Reset</button>
       </div>
     </div>
   );
-}
+};
