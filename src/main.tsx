d062264c1e32b43095e83eaabb3f4d4a609036bf
Inma Gijón <igijoninf@gmail.com>
2026-01-13 13:26:13 +0100

    Preparando despliegue gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { GifsApp } from "./GifsApp";
+import { BrowserRouter } from "react-router-dom";
+
 // import { MyCounterApp } from "./counter/components/MyCounterApp";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <GifsApp />
-    {/* <MyCounterApp /> */}
+    <BrowserRouter basename="/react_GifExpertApp/">
+      <GifsApp />
+      {/* <MyCounterApp /> */}
+    </BrowserRouter>
   </StrictMode>
 );
