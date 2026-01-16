5d4517a06b7b443c7b4045a948d53c4119235f23
Inma Gijón <igijoninf@gmail.com>
2026-01-16 08:38:40 +0100

    Esqueleto CustomHeaderTest

---
+++ src/shared/components/CustomHeader.test.tsx
@@ -0,0 +1,18 @@
+import {describe, test} from 'vitest';
+
+
+describe('CustomHeader', () => {
+    test('should render the title correctly', () => {
+        
+    });
+    test('should render the description when provided', () => {
+        
+    });
+    
+    test('should not render description when not provided', () => {
+        
+    });
+
+
+
+});
